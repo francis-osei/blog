8d63177aba472b912c267df4d15ed8fee141aced
francis-osei <oseifrancis673@gmail.com>
2025-09-14 23:41:58 -0700

    refactor(userdto): enhance UpdateUserRole dto with validation

--- src/users/dto/updateUserRole.ts
+++ src/users/dto/updateUserRole.ts
@@ -1,7 +1,12 @@
 import { Role } from '@prisma/client';
-import { IsEnum } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsEnum, IsNotEmpty } from 'class-validator';
 
 export class UpdateUserRole {
-  @IsEnum(Role, { message: 'Role must be USER ADMIN or AUTHOR' })
+  @IsNotEmpty({ message: 'Role is required' })
+  @IsEnum(Role, {
+    message: `Role must be one of: ${Object.values(Role).join(', ')}`,
+  })
+  @Transform(({ value }) => value?.toString().trim().toUpperCase())
   role: Role;
 }
